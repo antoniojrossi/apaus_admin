cb8a260d79f7b92ecf179974ae6ebc701ffcf5b1
Antonio J Rossi <antoniojrossi@gmail.com>
2016-11-25 22:58:10 +0100

    Añadida configuración para que la salida de los tests tenga formato JUnit.

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,13 @@ config :logger, level: :warn
 # Configure your database
 config :apaus_admin, ApausAdmin.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "antoniojrossi",
+  password: "",
   database: "apaus_admin_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure test junit formatter
+config :junit_formatter,
+  report_file: "results.xml",
+  print_report_file: true
